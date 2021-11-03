24a81ea85fc9e8ed9f3d440f9be59629260f1e66
Alesya <kyzya77777@gmail.com>
2021-11-03 16:37:30 +0300

    Выполнила задание 2

---
+++ src/components/wrapper.js
@@ -0,0 +1,12 @@
+const createTripWrapper = () => {
+  return (
+    `<ul class="trip-days">
+      <li class="trip-days__item  day">
+        
+        <ul class="trip-events__list"></ul>
+      </li>
+    </ul>`
+  );
+};
+
+export {createTripWrapper};
